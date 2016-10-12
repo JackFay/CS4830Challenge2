e2c80eb108f82d984f59d06c9d7fc1f45d16190e
Jack Fay <Jack Fay>
2016-10-12 13:30:27 -0500

    First Commit

---
+++ IService1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfREST
+{
+    [ServiceContract]
+    public interface IService1
+    {
+
+        //These are the Operation Contracts used to format the HTTP requests
+        [OperationContract]
+        [WebGet(UriTemplate = "Teams", ResponseFormat = WebMessageFormat.Json)]
+        IEnumerable<Team> GetTeams();
+
+        [OperationContract]
+        [WebInvoke(
+            Method = "GET",
+            UriTemplate = "Teams/{name}",
+            ResponseFormat = WebMessageFormat.Json)]
+        Team GetTeam(string name);
+
+        [OperationContract]
+        [WebInvoke(
+            Method = "POST",
+            UriTemplate = "Teams",
+            BodyStyle = WebMessageBodyStyle.Wrapped,
+            RequestFormat = WebMessageFormat.Json,
+            ResponseFormat = WebMessageFormat.Json)]
+        void AddTeam(String Name, String City, Stadium stadium);
+
+        [OperationContract]
+        [WebInvoke(
+            Method = "PUT",
+            UriTemplate = "Teams",
+            BodyStyle = WebMessageBodyStyle.Wrapped,
+            RequestFormat = WebMessageFormat.Json,
+            ResponseFormat = WebMessageFormat.Json)]
+        void UpdateTeam(String Name, String City, Stadium stadium);
+
+        [OperationContract]
+        [WebInvoke(
+            Method = "DELETE",
+            UriTemplate = "Teams",
+            BodyStyle = WebMessageBodyStyle.Wrapped,
+            RequestFormat = WebMessageFormat.Json,
+            ResponseFormat = WebMessageFormat.Json)]
+        void DeleteTeams();
+
+        [OperationContract]
+        [WebInvoke(
+            Method = "GET",
+            UriTemplate = "Teams/{name}/Players",
+            ResponseFormat = WebMessageFormat.Json)]
+        IEnumerable<Player> GetTeamPlayers(string name);
+
+
+        [OperationContract]
+        [WebInvoke(
+            Method = "GET",
+            UriTemplate = "Teams/{name}/Stadium",
+            ResponseFormat = WebMessageFormat.Json)]
+        Stadium GetTeamStadium(string name);
+
+        [OperationContract]
+        [WebInvoke(
+            Method = "POST",
+            UriTemplate = "Teams/{name}/Players",
+            BodyStyle = WebMessageBodyStyle.Bare,
+            RequestFormat = WebMessageFormat.Json,
+            ResponseFormat = WebMessageFormat.Json)]
+        void AddTeamPlayers(string name, List<Player> players);
+
+    }
+
+
+    //The data contracts, aka all the different objects
+    [DataContract]
+    public class Team
+    {
+        [DataMember]
+        public string City { get; set; }
+        [DataMember]
+        public string Name { get; set; }
+        [DataMember]
+        public Stadium HomeStadium { get; set; }
+        [DataMember]
+        public List<Player> Players { get; set; }
+
+    }
+
+    [DataContract]
+    public class Player
+    {
+        [DataMember]
+        public string FirstName { get; set; }
+        [DataMember]
+        public string LastName { get; set; }
+        [DataMember]
+        public int Age { get; set; }
+        [DataMember]
+        public decimal Salary { get; set; }
+
+    }
+
+    [DataContract]
+    public class Stadium
+    {
+        [DataMember]
+        public string Name { get; set; }
+        [DataMember]
+        public int Capacity { get; set; }
+        [DataMember]
+        public decimal TicketPrice { get; set; }
+    }
+}
---
+++ Service1.svc.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfREST
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+
+        //In memory data structure to hold all of the teams
+        static List<Team> teams = new List<Team>();
+
+
+        public IEnumerable<Team> GetTeams()
+        {
+            return teams;
+        }
+
+        public Team GetTeam(string name)
+        {
+            //Checking to see if team exists
+            Team t = teams.Find(x => x.Name == name);
+            if (t == null) return null;
+            return t;
+        }
+
+        public void AddTeam(string Name, string City, Stadium stadium)
+        {
+            if (teams.Find(x => x.Name == Name) != null) return;
+            //creating the new team and setting attributes
+            Team team = new Team();
+            team.Name = Name;
+            team.City = City;
+            team.HomeStadium = stadium;
+            team.Players = new List<Player>();
+
+            teams.Add(team);
+        }
+
+        public void UpdateTeam(string Name, string City, Stadium stadium)
+        {
+            //Checking to see if team exists
+            Team t = teams.Find(x => x.Name == Name);
+            if (t == null) return;
+            t.City = City;
+            t.HomeStadium = stadium;
+        }
+
+        public void DeleteTeams()
+        {
+            teams.Clear();
+        }
+
+        public IEnumerable<Player> GetTeamPlayers(string name)
+        {
+            //Checking to see if team exists
+            Team t = teams.Find(x => x.Name == name);
+            if (t == null) return null;
+
+            return t.Players;
+        }
+
+        public Stadium GetTeamStadium(string name)
+        {
+            //Checking to see if team exists
+            Team t = teams.Find(x => x.Name == name);
+            if (t == null) return null;
+
+            return t.HomeStadium;
+        }
+
+        public void AddTeamPlayers(string name, List<Player> players)
+        {
+            //Checking to see if team exists
+            Team t = teams.Find(x => x.Name == name);
+            if (t == null) return;
+            foreach (var p in players)
+            {
+                t.Players.Add(p);
+            }
+
+
+
+        }
+    }
+}
